b0c4c8bce271d58dfa27a48942bf735764676fa3
Sergey <s.petrychenko@gmail.com>
2016-06-22 18:54:50 +0300

    The Task PDO with changes

--- index.php
+++ index.php
@@ -5,24 +5,19 @@ require_once 'LoggerInFileSystem.php';
 $message = "I'm a message";
 
 /**Use logger to write log into DB
-
+*/
 $LoggerInDB = new LoggerInDB();
 
 $LoggerInDB->error($message);
 $LoggerInDB->warning($message);
 $LoggerInDB->notice($message);
- */
+
 /**Use logger to write log into file
- *
  */
-    $LoggerInFileSystem = new LoggerInFileSystem(DSN, NAME, PASSWORD);
+$LoggerInFileSystem = new LoggerInFileSystem();
 
-    $LoggerInFileSystem->error($message);
-    $LoggerInFileSystem->warning($message);
-    $LoggerInFileSystem->notice($message);
+$LoggerInFileSystem->error($message);
+$LoggerInFileSystem->warning($message);
+$LoggerInFileSystem->notice($message);
 
-/**
- * Close DB connection
- */
-$dbh = null;
 ?>
---
+++ readme.txt
@@ -0,0 +1,7 @@
+index.php - The file implements either write to DB or file
+LoggerInDB.php - The file implements to write message in DB
+LoggerInFileSystem.php - The file implements to write message in file
+Config.php - The file contains information to login into DB
+log.txt - The file with log messages
+LoggerInterface.php - The file contains interface for log app
+LoggerAbstract.php - The file contains abstract class and implement interface
