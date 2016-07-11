bd7f5e726e0f55202f3b0a473bd6463af0238719
Sergey <s.petrychenko@gmail.com>
2016-07-11 10:16:17 +0300

    add sql queries

---
+++ sql/SQL_QUERIES.sql
@@ -0,0 +1,20 @@
+SELECT emp.id, emp.name, emp.surname, pay.amount
+FROM employees as emp
+JOIN payments as pay
+ON emp.id = pay.id_empl
+WHERE pay.amount = (SELECT MAX(amount) FROM payments) AND pay.payment_date = '2016-06-10';
+
+SELECT emp.id, emp.name, emp.surname, pay.amount
+FROM employees as emp
+JOIN payments as pay
+ON emp.id = pay.id_empl
+WHERE pay.amount = (SELECT MAX(amount) FROM payments) AND pay.payment_date >= '2016-04-01' AND pay.payment_date < '2016-07-01' LIMIT 1;
+
+SELECT SUM(amount) FROM payments WHERE payment_date >= '2016-06-01' AND payment_date < '2016-07-01';
+
+SELECT name, surname, amount
+FROM employees as e
+JOIN payments as p
+ON e.id = p.id_empl
+WHERE (SELECT MAX(amount) FROM payments WHERE amount < (SELECT MAX(amount) FROM payments)) LIMIT 1
+;
