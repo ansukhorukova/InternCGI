fb1a40dd54fbdf6e1d2f3f0b3db156657151d50b
Sergey <s.petrychenko@gmail.com>
2016-07-12 09:17:36 +0300

    task3: start

---
+++ app/Spet/Src/ManagePanel/AuthorizationForm.php
@@ -0,0 +1,42 @@
+<?php
+namespace Spet\ManagePanel;
+
+class AuthorizationForm
+{
+    public function __construct()
+    {
+        echo <<<LOGIN
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container" style="margin-top: 20px; padding: 10px; width: 550px; box-shadow: 0 0 10px rgba(0,0,150,0.5);">
+  <h3>Authorization to the products management panel</h3>
+  <form role="form" method="post" action="Authorization.php">
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="password">
+    </div>
+    <div class="checkbox">
+      <label><input type="checkbox" name="remember"> Remember me</label>
+    </div>
+    <button type="submit" class="btn btn-default">Submit</button>
+  </form>
+</div>
+
+</body>
+</html>
+LOGIN;
+    }
+}
+
